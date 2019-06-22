87d1d06337e98f1b2a6cd229d6daf253e02de790
Felipe <felipe@MacBook-Pro-de-Felipe.local>
2019-06-22 19:31:19 -0300

    Iniciando com redux

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,31 +1,36 @@
 import * as React from 'react';
-
+import { createStore } from 'redux';
+import reducer from '../reducers';
 
 export default class List extends React.Component<any, any>{
+    
+    initialState = {
+        users: [
+            { name: "Felipe", id: 1 },
+            { name: "Adalberto", id: 2 },
+            { name: "Gustavo", id: 3},
+            { name: "Julia", id: 4}
+        ]
+    };
+
+    store = createStore(reducer, this.initialState as any);
+    
     constructor(props: any) {
         super(props);
 
-        this.state = {
-            List: [
-                { name: "Felipe", id: 1 },
-                { name: "Adalberto", id: 2 },
-                { name: "Gustavo", id: 3}
-            ]
-        };
     }
 
 
     render() {
-
         return (
             <div>
-                {this.state.List.map(
+                {this.store.getState().users.map(
                     (user: any) => {
                         return (
                             <h4 key={user.id} >{user.name}</h4>
                         );
                     }
-                ) }
+                )}
             </div>
         );
     }
---
+++ src/reducers/index.tsx
@@ -0,0 +1,5 @@
+
+
+export default (state: any) => {
+    return state;
+}
